f26e9d93587e8386b85e063ace7faf14ae685968
Julis <julis@thegreenshot.green>
2024-06-09 20:55:22 +0200

    password not the same

--- pages/authentications/register/student/password.vue
+++ pages/authentications/register/student/password.vue
@@ -1,73 +1,3 @@
-<template>
-  <LayoutAuthentication :title=" $t('register_enter_password')" >
-    <!-- <form action="" class="w-full flex flex-col gap-2 pt-3">
-      <UForm :state="state" class="space-y-4" @submit="onSubmit">
-        <UFormGroup :label="$t(`auth_password`)" name="password">
-
-          <UInput autocomplete="off" size="lg" v-model="state.password" @focus="onFocusPassword()"
-                  @blur="onblurPassword()" name="password" :type="showPasswords ? 'password' : 'text'"
-                  :placeholder="$t(`auth_password_label`)" class="z-[1]">
-            <UButton tabindex="-1" v-if="showPasswords" @click="togglePassword()" tab-index="-1"
-                     icon="i-heroicons-eye" size="sm" color="primary" square variant="soft"
-                     class="absolute top-0 right-0 mt-1 mr-1" />
-            <UButton tabindex="-1" v-if="!showPasswords" @click="togglePassword()" tab-index="-1"
-                     icon="i-heroicons-eye-slash" size="sm" color="primary" square variant="soft"
-                     class="absolute top-0 right-0 mt-1 mr-1" />
-            <div class="password-check absolute w-full bg-white  bottom-50 h-[60px] rounded-lg  shadow-2xl	 p-2 border-2"
-                 v-if="isPasswordFocused">
-              <div class="flex flex-col gap-2 w-full">
-                <UProgress :value="passwordComplexity" :indicator="false" />
-                <div class="text-lg lg:text-sm xl:text-sm 2xl:text-sm">
-                  {{ $t("complexity") }} : {{ passwordComplexity }}%
-                </div>
-              </div>
-            </div>
-          </UInput>
-        </UFormGroup>
-        <UFormGroup :label="$t(`auth_password_confirmation`)" name="password-confirm">
-          <UInput autocomplete="off" size="lg" v-model="state.passwordConfirm"
-                  :type="showPasswords ? 'password' : 'text'"
-                  :placeholder="$t(`auth_password_confirmation_label`)">
-            <UButton tabindex="-1" v-if="showPasswords" @click="togglePassword()" icon="i-heroicons-eye"
-                     size="sm" color="primary" square variant="soft"
-                     class="absolute top-0 right-0 mt-1 mr-1" />
-            <UButton tabindex="-1" v-if="!showPasswords" @click="togglePassword()"
-                     icon="i-heroicons-eye-slash" size="sm" color="primary" square variant="soft"
-                     class="absolute top-0 right-0 mt-1 mr-1" />
-          </UInput>
-        </UFormGroup>
-
-        <div
-            class="absolute left-0 bottom-0 lg:relative xl:relative 2xl:relative p-3 lg:p-0 xl:p-0 2xl:p-0 w-full">
-          <UButton size="lg" type="submit" class="bg-color-main hover:bg-green-500" block>
-            {{ $t(`continue`) }}
-          </UButton>
-        </div>
-
-      </UForm>
-    </form> -->
-    <form class="space-y-6" @submit="onSubmit">
-      <FormField v-slot="{ componentField }" name="password" :validate-on-blur="!isFieldDirty">
-        <FormItem>
-          <FormLabel>Mots de passe</FormLabel>
-          <FormControl v-bind="componentField">
-            <CommonFormInput type="password" placeholder="Votre mots de passe" />
-          </FormControl>
-        </FormItem>
-      </FormField>
-      <FormField v-slot="{ componentField }" name="passwordConfirm" :validate-on-blur="!isFieldDirty">
-        <FormItem>
-          <FormLabel>Confirmer votre mots de passe</FormLabel>
-          <FormControl v-bind="componentField">
-            <CommonFormInput type="password" placeholder="Confirmation de votre mots de passe" />
-          </FormControl>
-        </FormItem>
-      </FormField>
-      <CommonFormSubmit />
-    </form>
-    
-  </LayoutAuthentication>
-</template>
 <script setup lang="ts">
 
 import { toTypedSchema } from '@vee-validate/zod';
@@ -79,28 +9,28 @@ import { LocalStorageGetItem } from '~/scripts/local-storage';
 import { passwordStore } from '~/stores/password.store';
 import { toast } from '@/components/ui/toast/use-toast'
 
-
 const validation = useValidation();
 const loadingStore = useLoadingStore();
 loadingStore.hide();
-// const toast = useToast();
+
 
 let showPasswords = ref(true);
 let isPasswordFocused = ref(false);
 const store = passwordStore();
 function togglePassword() {
-    showPasswords.value = !showPasswords.value;
+  showPasswords.value = !showPasswords.value;
 }
 
-
 const formSchema = toTypedSchema(z.object({
-  password: z.string().min(5), 
+  password: z.string().min(5),
   passwordConfirm: z.string().min(5),
 }));
 
-const { isFieldDirty, handleSubmit } = useForm({
+const { isFieldDirty, handleSubmit, values } = useForm({
   validationSchema: formSchema,
 });
+
+
 const onSubmit = handleSubmit( async (values) => {
   loadingStore.show();
   try{
@@ -115,82 +45,30 @@ const onSubmit = handleSubmit( async (values) => {
   loadingStore.hide();
 })
 
-// const state = reactive({
-//     password: '',
-//     passwordConfirm: ''
-// })
-// function onFocusPassword() {
-//     isPasswordFocused.value = true;
-// }
-// function onblurPassword() {
-//     isPasswordFocused.value = false;
-// }
-// const passwordComplexity = computed(() => {
-//     return validation.CheckPasswordComplexity(state.password);
-// });
-
-// async function onSubmit() {
-//   loadingStore.show();
-//   try
-//   {
-//     store.setPassword()
-//   }
-
-//     // if (!validation.checkPasswords(state.password, state.passwordConfirm)) {
-//     //     toast.add(
-//     //         {
-//     //             id: "1",
-//     //             title: 'Validation',
-//     //             description: 'Veillez vérifiez votre mots de passe!',
-//     //             icon: "i-heroicons-check-badge",
-//     //             color: "red",
-//     //             ui: {
-//     //                 background: "bg-red-100"
-//     //             }
-//     //         }
-//     //     )
-//     //     return;
-//     // }
-//     // if (state.password.length < 5) {
-//     //     toast.add(
-//     //         {
-//     //             id: "1",
-//     //             title: 'Validation',
-//     //             description: 'Mots de passe trop courte!',
-//     //             icon: "i-heroicons-check-badge",
-//     //             color: "red",
-//     //             ui: {
-//     //                 background: "bg-red-100"
-//     //             }
-//     //         }
-//     //     )
-//     //     return;
-//     // }
-
-//     // useGqlToken({
-//     //     token: `${LocalStorageGetItem(environment.auth_token)}`,
-//     //     config: {
-//     //         type: 'Bearer',
-//     //         name: 'Authorization'
-//     //     }
-//     // });
-//    // loadingStore.show();
-//     // GqlNewPassword({ password: { Hash: state.password } }).then(response => {
-//     //     navigateTo("/authentications/register/student/about");
-//     // }, error => {
-//     //     loadingStore.hide();
-//     //     toast.add(
-//     //         {
-//     //             id: "1",
-//     //             title: 'Erreur!',
-//     //             description: 'Une erreur est survenue pendant l\'opérations!',
-//     //             icon: "i-heroicons-exclamation-triangle",
-//     //             color: "red",
-//     //             ui: {
-//     //                 background: "bg-red-100"
-//     //             }
-//     //         }
-//     //     )
-//     // });
-// }
 </script>
+<template>
+  <LayoutAuthentication :title=" $t('register_enter_password')" >
+    <div v-if="values.password !== values.passwordConfirm" class="text-red-700 py-5">
+      *PASSWORD NOT THE SAME
+    </div>
+    <form class="space-y-6" @submit="onSubmit">
+      <FormField v-slot="{ componentField }" name="password" :validate-on-blur="!isFieldDirty">
+        <FormItem>
+          <FormLabel>Mots de passe</FormLabel>
+          <FormControl v-bind="componentField">
+            <CommonFormInput type="password" placeholder="Votre mot de passe" />
+          </FormControl>
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField }" name="passwordConfirm" :validate-on-blur="!isFieldDirty">
+        <FormItem>
+          <FormLabel>Confirmez votre mot de passe</FormLabel>
+          <FormControl v-bind="componentField">
+            <CommonFormInput type="password" placeholder="Confirmation de votre mots de passe" />
+          </FormControl>
+        </FormItem>
+      </FormField>
+      <CommonFormSubmit />
+    </form>
+  </LayoutAuthentication>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,5 @@
 <!--  <div class="w-screen h-screen">-->
 <!--&lt;!&ndash;    <landing/>&ndash;&gt;-->
 <!--  </div>-->
-<landing/>
+<!--<landing/>-->
 </template>
